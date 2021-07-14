dd6fa64059f0bd84b537ac0220445cd16d1cd2be
lewy192 <lewis.james.hill@outlook.com>
2021-07-14 19:52:30 +1000

    add:header component and styled

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./About.css";
+
+function Header() {
+    return (
+        <div className="nav-bar">
+            <h2 className="brand">Lernantino</h2>
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <a href="/about" className="nav-link">
+                        about
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a href="/portfolio" className="nav-link">
+                        portfolio
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a href="/contact" className="nav-link">
+                        contact
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a href="/resume" className="nav-link"></a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
