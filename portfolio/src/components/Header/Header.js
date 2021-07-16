09cccd0d30ef857ace49f649a2ee40e59fedb427
Lewis Hill <lewis.james.hill@outlook.com>
2021-07-16 20:59:42 +1000

    add: nav bar transform

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -4,41 +4,68 @@ import "./Header.css";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function Header() {
-    const [className, setClassName] = useState("");
+    const [className, setClassName] = useState(false);
 
     return (
         <div className="side-navbar">
             <Link to="/" className="brand-link">
                 <h2 className="brand">L H</h2>
             </Link>
-            <nav className="nav-bar">
+            <nav className={`nav-bar ${className ? "transformed" : ""}`}>
                 <ul className="nav-list nav-list1">
-                    <li className="list-item list-item1">
+                    <li
+                        className={`list-item list-item1 ${
+                            className ? "transform" : ""
+                        }`}
+                    >
                         <Link to="/about" className="nav-link">
                             about
                         </Link>
                     </li>
-                    <li className="list-item list-item2">
+                    <li
+                        className={`list-item list-item2 ${
+                            className ? "transform" : ""
+                        }`}
+                    >
                         <Link to="/portfolio" className="nav-link">
                             portfolio
                         </Link>
                     </li>
-                    <li className="list-item list-item3">
+                    <li
+                        className={`list-item list-item3 ${
+                            className ? "transform" : ""
+                        }`}
+                    >
                         <Link to="/contact" className="nav-link">
                             contact
                         </Link>
                     </li>
-                    <li className="list-item list-item4">
+                    <li
+                        className={`list-item list-item4 ${
+                            className ? "transform" : ""
+                        }`}
+                    >
                         <Link to="/resume" className="nav-link">
                             resume
                         </Link>
                     </li>
                 </ul>
             </nav>
-            <div className="burger-menu">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
+            <div
+                className="burger-menu"
+                onClick={() => {
+                    className ? setClassName(false) : setClassName(true);
+                }}
+            >
+                <div
+                    className={`line1 ${className ? "transformed" : ""}`}
+                ></div>
+                <div
+                    className={`line2 ${className ? "transformed" : ""}`}
+                ></div>
+                <div
+                    className={`line3 ${className ? "transformed" : ""}`}
+                ></div>
             </div>
         </div>
     );
--- portfolio/src/components/Pages/Main/Main.js
+++ portfolio/src/components/Pages/Main/Main.js
@@ -39,39 +39,6 @@ function Main() {
                 Chat With Me
             </a>
         </section>
-
-        // <div className="main-container">
-        //     <div
-        //         className="profile-container"
-        //         id="profile-container"
-        //         style={{ backgroundImage: `url(${melbourne})` }}
-        //     >
-        //         <div className="profile-img-container">
-        //             <div
-        //                 className="profile-img"
-        //                 style={{
-        //                     backgroundImage: `url(${portrait})`,
-        //                 }}
-        //             ></div>
-        //             <div className="profile-description-container">
-        //                 <h3 className="small-intro">Full Stack Developer</h3>
-        //                 <h4 className="location-intro">Melbourne, Australia</h4>
-        //                 <h3 className="name-head">
-        //                     Hey I'm Lewis, My passion is Full Stack Web
-        //                     Development and problem solving.
-        //                     <br />
-        //                     <br />
-        //                     Building has always been my specialty from lego to
-        //                     motorbikes and everything in between.
-        //                 </h3>
-        //             </div>
-        //         </div>
-        //         <div className="color">
-        //             <h1 className="type-writer"></h1>
-        //             <p className="quoter">A Few Quotes That I Live By</p>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
