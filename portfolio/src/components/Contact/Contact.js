e4d99eb4bbf0d44f47fe52e411ae1858df459541
Lewis Hill <lewis.james.hill@outlook.com>
2021-07-23 13:06:06 +1000

    add:contact me form

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./Contact.css";
 
 function Contact() {
-    return <div> this is a test</div>;
-}
+    const [name, setUsersName] = useState();
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="contact-container">
+            <p className="contact-me-para"></p>
+            <h2 className="contact-me">&lt;Contact Me/&gt;</h2>
+            <form className="contact-form" onSubmit={sendEmail}>
+                <label htmlFor="name-field" className="name-label">
+                    Name:
+                </label>
+                <input type="text" id="name-field" name="fullName" />
+                <label htmlFor="contact-field" className="contact-label">
+                    Contact Details:
+                </label>
+                <input type="text" id="contact-field" name="contactDetails" />
+                <label htmlFor="message-field" className="message-label">
+                    Message:
+                </label>
+                <textarea
+                    name="message"
+                    id="message-field"
+                    cols="30"
+                    rows="10"
+                ></textarea>
+                <button type="submit" className="submit button">
+                    Send
+                </button>
+            </form>
+            <h2 className="contact-me">&lt;/Contact Me&gt;</h2>
+        </div>
+    );
+}
 export default Contact;
--- portfolio/src/components/Project/Project.js
+++ portfolio/src/components/Project/Project.js
@@ -16,24 +16,31 @@ import { motion } from "framer-motion";
 // </div>`
 function Project({ project }) {
     const [ref, inView] = useInView({
-        triggerOnce: true,
+        // triggerOnce: true,
         threshold: 0.5,
     });
 
     return (
-        <div className={`project `} ref={ref}>
+        <div className="project" ref={ref}>
             <div className="photo"></div>
             <div className="description-container">
                 <h3 className="project-header">TechREADY</h3>
-                <motion.div
-                    className={`front-box ${inView ? "animate" : ""}`}
-                ></motion.div>
-                <motion.div className={`back-box ${inView ? "animate" : ""}`}>
+                <motion.div className={`front-box ${inView ? "animate" : ""}`}>
+                    <p className="project-description">
+                        this project is awesome, check it out
+                    </p>
+                </motion.div>
+                {/* <motion.div className={`back-box ${inView ? "animate" : ""}`}>
                     <p className="project-description">
                         this porject was awesome because...
                     </p>
-                </motion.div>
-                <h3 className="tech-list">JWT, Node RESTful,</h3>
+                </motion.div> */}
+                <ul className="tech-list">
+                    <li>Node</li>
+                    <li>JWT</li>
+                    <li>REST API</li>
+                    <li>Full - Stack</li>
+                </ul>
             </div>
         </div>
     );
