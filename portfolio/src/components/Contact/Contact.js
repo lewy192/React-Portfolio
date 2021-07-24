79991bd8460a36d6b905dfc4e1fb34da39c57e6a
lewy192 <lewis.james.hill@outlook.com>
2021-07-24 11:28:32 +1000

    add:contact page complete

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Paperplane from "./Paperplane";
+
 import "./Contact.css";
 
 function Contact() {
@@ -10,16 +12,22 @@ function Contact() {
     };
 
     return (
-        <div className="contact-container">
-            <p className="contact-me-para"></p>
+        <section className="contact-container">
             <h2 className="contact-me">&lt;Contact Me/&gt;</h2>
+            <p className="contact-me-para">
+                Feel free to reach out to me at{" "}
+                <a href="mailto:lewis.james.hill@outlook.com">
+                    lewis.james.hill@outlook.com
+                </a>{" "}
+                or drop me a mail bellow.
+            </p>
             <form className="contact-form" onSubmit={sendEmail}>
                 <label htmlFor="name-field" className="name-label">
                     Name:
                 </label>
                 <input type="text" id="name-field" name="fullName" />
                 <label htmlFor="contact-field" className="contact-label">
-                    Contact Details:
+                    Email:
                 </label>
                 <input type="text" id="contact-field" name="contactDetails" />
                 <label htmlFor="message-field" className="message-label">
@@ -29,14 +37,14 @@ function Contact() {
                     name="message"
                     id="message-field"
                     cols="30"
-                    rows="10"
+                    rows="3"
                 ></textarea>
-                <button type="submit" className="submit button">
-                    Send
+                <button type="submit" className="submit-button">
+                    <Paperplane />
                 </button>
             </form>
             <h2 className="contact-me">&lt;/Contact Me&gt;</h2>
-        </div>
+        </section>
     );
 }
 export default Contact;
